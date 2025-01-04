e224fadf2d9b34cd768999083213c3d61ed6ea2b
Wombosvideo <luca.bosin@heviat.com>
2025-01-04 17:00:18 +0100

    Add mode-watcher for automatic dark mode

--- setup/src/routes/+layout.svelte
+++ setup/src/routes/+layout.svelte
@@ -2,12 +2,16 @@
   import { i18n } from '$lib/i18n';
   import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 
+  import { ModeWatcher } from 'mode-watcher';
+
   import '../app.css';
   import '@fontsource-variable/inter';
   import '@fontsource-variable/source-code-pro';
 
   let { children } = $props();
 </script>
+
+<ModeWatcher />
 
 <ParaglideJS {i18n}>
   {@render children()}
