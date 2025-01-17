9ba28ab8aa511bbb675b6038925bd955d335f0ff
Wombosvideo <luca.bosin@heviat.com>
2025-01-17 19:40:05 +0100

    Remove uneeded media query parentheses

--- setup/src/lib/hooks/is-mobile.svelte.ts
+++ setup/src/lib/hooks/is-mobile.svelte.ts
@@ -4,6 +4,6 @@ const MOBILE_BREAKPOINT = 768;
 
 export class IsMobile extends MediaQuery {
   constructor() {
-    super(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`, false);
+    super(`max-width: ${MOBILE_BREAKPOINT - 1}px`, false);
   }
 }
